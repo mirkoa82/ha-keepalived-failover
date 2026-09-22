32aea38a876bbb1075c95855711694536b9130f0
mirkoa82 <76661378+mirkoa82@users.noreply.github.com>
2026-09-22 13:32:31 +0200

    Create run.sh

---
+++ keepalived-failover/rootfs/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+bashio::log.info "Keepalived HA Failover avviato correttamente."
+bashio::log.info "Test di riconoscimento repository/add-on riuscito."
+
+while true; do
+  sleep 3600
+done
